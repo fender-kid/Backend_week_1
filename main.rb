5a37a181f8ee89bb9887f691d4066b009c18020c
fender-kid <jtt.sides@gmail.com>
2024-01-05 18:08:52 -0600

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+puts "Enter the length of your rectangle:"
+rectange_length = gets.chomp.to_f
+
+puts "Enter the width of your rectangle:"
+rectangle_width = gets.chomp.to_f
+
+rectangle_area = rectange_length * rectangle_width
+print "The area of your rectangle is #{rectangle_area}.\n"
