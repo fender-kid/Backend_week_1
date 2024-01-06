9061b61ad465092a5c86f454819f59139e32debe
fender-kid <jtt.sides@gmail.com>
2024-01-06 08:07:01 -0600

    exercise 2

---
+++ exercises.rb
@@ -0,0 +1,30 @@
+=begin 
+Practice Problem
+Declare a local variable called my_name and assign it your full name as a string. 
+Declare a constant variable called PI and assign it the value of pi (3.14).
+Print both variables to the terminal.
+=end
+
+my_name = "James Sides"
+
+PI = 3.14
+
+print my_name
+
+print PI
+
+=begin
+Practice Problem
+Write a Ruby script that calculates and prints the area of a rectangle with a width 
+of 10 and a height of 5.
+
+Write a Ruby script that combines a string and a number. It should create a string 
+that includes a number in it through interpolation and then print it.
+=end
+
+
+rectange_height = 5
+rectangle_width = 10
+
+rectangle_area = rectange_height * rectangle_width
+puts "\nThe area of your rectangle is #{rectangle_area}.\n"
